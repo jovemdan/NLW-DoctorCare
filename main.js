87930b86f471583208ef4b5766ce624e9aa3b824
jovemdan <danilogdev@gmail.com>
2022-05-25 18:07:08 -0300

    feat: menu

---
+++ main.js
@@ -0,0 +1,12 @@
+function onScroll() {
+    
+    if(scrollY > 0)
+        navigation.classList.add("scroll")
+    else
+         navigation.classList.remove("scroll")
+}
+
+function onScroll() {
+    
+}
+
