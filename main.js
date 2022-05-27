90721fc43e601d69af7e2362491f2f1655eb90c6
jovemdan <danilogdev@gmail.com>
2022-05-27 15:30:39 -0300

    feat: finalizando menu

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 function onScroll() {
-    
-    if(scrollY > 0)
-        navigation.classList.add("scroll")
-    else
-         navigation.classList.remove("scroll")
+    if (scrollY > 0) {
+        document.getElementById('navigation').classList.add('scroll')
+    } else {
+        document.getElementById('navigation').classList.remove('scroll')
+    }
+  }
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
 }
 
-function onScroll() {
-    
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
 }
 
