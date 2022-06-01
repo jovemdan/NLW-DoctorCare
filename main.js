4513c935f3a07914e629754ff25291422d26d9f2
jovemdan <danilogdev@gmail.com>
2022-06-01 15:39:28 -0300

    feat: section services e about

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 function onScroll() {
-    if (scrollY > 0) {
-        document.getElementById('navigation').classList.add('scroll')
-    } else {
-        document.getElementById('navigation').classList.remove('scroll')
-    }
+  if (scrollY > 0) {
+    document.getElementById('navigation').classList.add('scroll');
+  } else {
+    document.getElementById('navigation').classList.remove('scroll');
   }
-
-function openMenu(){
-    document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
-    document.body.classList.remove('menu-expanded')
+function openMenu() {
+  document.body.classList.add('menu-expanded');
 }
 
-ScrollReveal().reveal('#home');
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+#services,
+#services .card,
+#about,
+#about header,
+#about .content`);
